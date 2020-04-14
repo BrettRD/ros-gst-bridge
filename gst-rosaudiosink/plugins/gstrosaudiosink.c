1e1cfeeaa7258342f8c32fc670405d0c5b7c21c7
BrettRD <brettrd@brettrd.com>
2020-04-14 18:04:19 +1000

    gst compiles

--- gst-rosaudiosink/plugins/gstrosaudiosink.c
+++ gst-rosaudiosink/plugins/gstrosaudiosink.c
@@ -248,35 +248,3 @@ gst_rosaudiosink_reset (GstAudioSink * sink)
 
 }
 
-static gboolean
-plugin_init (GstPlugin * plugin)
-{
-
-  /* FIXME Remember to set the rank if it's an element that is meant
-     to be autoplugged by decodebin. */
-  return gst_element_register (plugin, "rosaudiosink", GST_RANK_NONE,
-      GST_TYPE_ROSAUDIOSINK);
-}
-
-/* FIXME: these are normally defined by the GStreamer build system.
-   If you are creating an element to be included in gst-plugins-*,
-   remove these, as they're always defined.  Otherwise, edit as
-   appropriate for your external plugin package. */
-#ifndef VERSION
-#define VERSION "0.0.FIXME"
-#endif
-#ifndef PACKAGE
-#define PACKAGE "FIXME_package"
-#endif
-#ifndef PACKAGE_NAME
-#define PACKAGE_NAME "FIXME_package_name"
-#endif
-#ifndef GST_PACKAGE_ORIGIN
-#define GST_PACKAGE_ORIGIN "http://FIXME.org/"
-#endif
-
-GST_PLUGIN_DEFINE (GST_VERSION_MAJOR,
-    GST_VERSION_MINOR,
-    rosaudiosink,
-    "FIXME plugin description",
-    plugin_init, VERSION, "LGPL", PACKAGE_NAME, GST_PACKAGE_ORIGIN)
--- gst-rosaudiosink/plugins/gstrosaudiosinkplugin.c
+++ gst-rosaudiosink/plugins/gstrosaudiosinkplugin.c
@@ -48,14 +48,34 @@
 #include <gst/gst.h>
 #include "gstrosaudiosink.h"
 
+
 static gboolean
 plugin_init (GstPlugin * plugin)
 {
-  gst_element_register (plugin, "rosaudiosink", GST_RANK_NONE,
-      GST_TYPE_ROSAUDIOSINK);
 
-  return TRUE;
+  /* FIXME Remember to set the rank if it's an element that is meant
+     to be autoplugged by decodebin. */
+  return gst_element_register (plugin, "rosaudiosink", GST_RANK_NONE,
+      GST_TYPE_ROSAUDIOSINK);
 }
 
-GST_PLUGIN_DEFINE (GST_VERSION_MAJOR, GST_VERSION_MINOR, rosaudiosink, "FIXME Template plugin", plugin_init, VERSION, "LGPL",   /* FIXME */
-    "GStreamer", "http://gstreamer.net/")
+
+#ifndef VERSION
+#define VERSION "0.0.FIXME"
+#endif
+#ifndef PACKAGE
+#define PACKAGE "FIXME_package"
+#endif
+#ifndef PACKAGE_NAME
+#define PACKAGE_NAME "FIXME_package_name"
+#endif
+#ifndef GST_PACKAGE_ORIGIN
+#define GST_PACKAGE_ORIGIN "http://FIXME.org/"
+#endif
+
+GST_PLUGIN_DEFINE (GST_VERSION_MAJOR,
+    GST_VERSION_MINOR,
+    rosaudiosink,
+    "FIXME plugin description",
+    plugin_init, VERSION, "LGPL", PACKAGE_NAME, GST_PACKAGE_ORIGIN)
+
