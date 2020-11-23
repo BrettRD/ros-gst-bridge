5041c142bda43fb6f4405a8eb7ba848ab88dcf15
BrettRD <brettrd@brettrd.com>
2020-11-23 13:31:14 +1100

    move config to cmake package for symlink installs

--- gst_bridge/launch/example.launch.py
+++ gst_bridge/launch/example.launch.py
@@ -10,11 +10,12 @@ def generate_launch_description():
     config_filepath = launch.substitutions.LaunchConfiguration('config_filepath')
 
     return launch.LaunchDescription([
-        launch.actions.DeclareLaunchArgument('config_filename', default_value='example'),
+        #launch.actions.DeclareLaunchArgument('config_filename', default_value='example'),
+        launch.actions.DeclareLaunchArgument('config_filename', default_value='simpleparams'),
 
         launch.actions.DeclareLaunchArgument('config_filepath', default_value=[
             launch.substitutions.TextSubstitution(text=os.path.join(
-                get_package_share_directory('gst_pipeline'), 'config', '')),
+                get_package_share_directory('gst_bridge'), 'config', '')),
             config_filename, launch.substitutions.TextSubstitution(text='.config.yaml')]),
 
         launch_ros.actions.Node(
