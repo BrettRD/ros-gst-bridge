2042131944269c54978fd3f1b2f59cdd897c71f5
BrettRD <brettrd@brettrd.com>
2021-03-24 13:09:43 +1100

    fixed uninitialised depth warning, handle endianness

--- gst_bridge/src/rosimagesink.cpp
+++ gst_bridge/src/rosimagesink.cpp
@@ -282,14 +282,14 @@ static gboolean rosimagesink_setcaps (GstBaseSink * gst_base_sink, GstCaps * cap
       depth = depth/8;
       RCLCPP_ERROR(ros_base_sink->logger, "low bits per pixel");
     }
-
-    //endianness = format_info->endianness;
+    endianness = GST_VIDEO_FORMAT_INFO_IS_LE(format_info) ? G_LITTLE_ENDIAN : G_BIG_ENDIAN;
   }
   else
   {
     RCLCPP_ERROR(ros_base_sink->logger, "setcaps missing format");
     if(!gst_structure_get_int (caps_struct, "endianness", &endianness))
-        RCLCPP_ERROR(ros_base_sink->logger, "setcaps missing endianness");
+      RCLCPP_ERROR(ros_base_sink->logger, "setcaps missing endianness");
+    return false;
   }
 
 
