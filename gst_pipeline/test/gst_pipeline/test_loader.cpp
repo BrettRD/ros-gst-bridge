18b48559d43a91d32ddde3da3995f4181a18e9bd
BrettRD <brettrd@brettrd.com>
2023-05-01 15:27:21 +1000

    adds unit testing

---
+++ gst_pipeline/test/gst_pipeline/test_loader.cpp
@@ -0,0 +1,40 @@
+#include <gtest/gtest.h>
+
+#include <gst_pipes.h>
+
+
+
+TEST(Test, gst_pipeline_loader) {
+
+  std::unique_ptr<pluginlib::ClassLoader<gst_pipes::gst_pipes_plugin>> loader;
+
+  std::unordered_map<std::string, std::string> ros_plugin_types =
+  {
+    {"appsrc", "gst_pipes::gst_pipes_appsrc"},
+    {"appsink", "gst_pipes::gst_pipes_appsink"},
+    {"pause", "gst_pipes::gst_pipes_pause_srv"}
+  };
+
+  std::unordered_map<
+    std::string,
+    std::shared_ptr<gst_pipes::gst_pipes_plugin>
+  > element_handlers;
+
+  // instantiate the pluginlib classloader
+  loader = std::make_unique<
+    pluginlib::ClassLoader<gst_pipes::gst_pipes_plugin> >(
+      "gst_pipeline", "gst_pipes::gst_pipes_plugin");
+
+  EXPECT_NE(loader, nullptr);
+
+  // load the ros plugins, but don't initialise
+  for(auto name_type : ros_plugin_types){
+    // instantiate the pluginlib plugins
+    element_handlers[name_type.first] =
+      loader->createSharedInstance(name_type.second);
+
+    EXPECT_NE(element_handlers[name_type.first], nullptr);
+  }
+
+
+}
