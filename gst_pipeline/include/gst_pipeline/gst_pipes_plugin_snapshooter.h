77426ec10d6e956c7a5c125eb74fe51702c163c8
BrettRD <brettrd@brettrd.com>
2023-05-08 16:28:07 +1000

    adds a snapshooter plugin

---
+++ gst_pipeline/include/gst_pipeline/gst_pipes_plugin_snapshooter.h
@@ -0,0 +1,63 @@
+#ifndef GST_PIPELINE__GST_PIPES_PLUGIN_SNAPSHOOTER_H_
+#define GST_PIPELINE__GST_PIPES_PLUGIN_SNAPSHOOTER_H_
+
+#include <gst_bridge/gst_bridge.h>
+#include <gst_pipes_plugin_base.h>
+
+#include "rclcpp/rclcpp.hpp"
+#include <std_msgs/msg/empty.hpp>
+
+namespace gst_pipes
+{
+/*
+  This plugin inserts a callback into the sink pad of the target elenemt,
+  conditionally dropping frames according to some traffic on a ros subscription
+*/
+class gst_pipes_snapshooter : public gst_pipes_plugin
+{
+public:
+  // during init, we need to
+  //  fetch a param from the node for the element name of the target element
+  //  search the pipeline for the target element
+  //  insert a probe into the pipeline on the sink pad of the target element
+  void initialise(
+    std::string name,  // the config name of the plugin
+    std::shared_ptr<gst_bridge::node_interface_collection> node_if, GstElement * pipeline);
+
+
+  void trigger_sub_cb(const std_msgs::msg::Empty::SharedPtr msg);
+
+
+
+  // This callback is run inside the pad of the sink element, the return will be ok or drop.
+  //  This function needs to be static, we can pass a pointer to our logic in user-data
+  static GstPadProbeReturn gst_pad_probe_cb (GstPad *pad, GstPadProbeInfo *info,
+    gpointer user_data);
+
+
+private:
+
+  // wrap some data for a user_data pointer into the gstreamer callback
+  struct cb_user_data {
+    gst_pipes_snapshooter* this_ptr;
+    std::atomic_bool trigger;
+  };
+
+  // the name of the target element in the pipeline
+  std::string elem_name_;
+
+  std::string trigger_topic_;
+
+  // a pointer to the bridge elements in the pipeline
+  GstElement * bin_;
+
+  rclcpp::Subscription<std_msgs::msg::Empty>::SharedPtr trigger_sub_;
+
+  // A data type we can pass as pointer into the callback
+  struct cb_user_data cb_data;
+
+};
+
+}  // namespace gst_pipes
+
+#endif  //GST_PIPELINE__GST_PIPES_PLUGIN_SNAPSHOOTER_H_
---
+++ gst_pipeline/src/gst_pipes_plugin_snapshooter.cpp
@@ -0,0 +1,102 @@
+#include <gst_pipes_plugin_snapshooter.h>
+
+namespace gst_pipes
+{
+
+
+
+void gst_pipes_snapshooter::initialise(
+  std::string name,  // the config name of the plugin
+  std::shared_ptr<gst_bridge::node_interface_collection> node_if, GstElement * pipeline)
+{
+  name_ = name;
+  node_if_ = node_if;
+  pipeline_ = pipeline;
+  cb_data.this_ptr = this;
+  cb_data.trigger=false;
+
+  elem_name_ = node_if->parameters
+                 ->declare_parameter(
+                   name + ".element_name", rclcpp::ParameterValue("mysrc"),
+                   descr("the name of the source element inside the pipeline", true))
+                 .get<std::string>();
+
+  trigger_topic_ = node_if->parameters
+                 ->declare_parameter(
+                   name + ".trigger_topic", rclcpp::ParameterValue("snap"),
+                   descr("the topic name to listen for trigger events", true))
+                 .get<std::string>();
+
+
+  rclcpp::QoS qos = rclcpp::SensorDataQoS();
+
+  trigger_sub_ = rclcpp::create_subscription<std_msgs::msg::Empty>(
+    node_if_->parameters, node_if_->topics, trigger_topic_, qos,
+    std::bind(&gst_pipes_snapshooter::trigger_sub_cb, this, std::placeholders::_1));
+
+
+  if (GST_IS_BIN(pipeline_)) {
+    bin_ = gst_bin_get_by_name(GST_BIN_CAST(pipeline_), elem_name_.c_str());
+    if (bin_) {
+      RCLCPP_INFO(
+        node_if->logging->get_logger(), "plugin gst_pipes_snapshooter '%s' found '%s'", name_.c_str(),
+        elem_name_.c_str());
+
+      // find the src pad of the element
+      GstPad *pad = gst_element_get_static_pad (bin_, "src");
+      // attach our callback to whenever a buffer crosses the pad
+      gst_pad_add_probe (pad, GST_PAD_PROBE_TYPE_BUFFER,
+        (GstPadProbeCallback) gst_pipes_snapshooter::gst_pad_probe_cb, &cb_data, NULL);
+
+
+    }
+
+    else {
+      RCLCPP_ERROR(
+        node_if->logging->get_logger(),
+        "plugin gst_pipes_snapshooter '%s' failed to locate a gstreamer element called '%s'",
+        name_.c_str(), elem_name_.c_str());
+    }
+  } else {
+    RCLCPP_ERROR(
+      node_if->logging->get_logger(),
+      "plugin gst_pipes_snapshooter '%s' received invalid pipeline in initialisation", name_.c_str());
+  }
+}
+
+void gst_pipes_snapshooter::trigger_sub_cb(const std_msgs::msg::Empty::SharedPtr msg){
+    (void) msg;
+    cb_data.trigger=true;
+}
+
+GstPadProbeReturn
+gst_pipes_snapshooter::gst_pad_probe_cb (GstPad *pad, GstPadProbeInfo *info,
+  gpointer user_data)
+{
+  GstPadProbeReturn ret;
+
+  (void) pad;
+  (void) info;
+
+  struct cb_user_data* cb_data = (struct cb_user_data *) user_data;
+
+  if(cb_data->trigger == true) {
+    ret = GST_PAD_PROBE_OK;
+  } else {
+    ret = GST_PAD_PROBE_DROP;
+  }
+  
+
+  cb_data->trigger=false;
+  return ret;
+}
+
+
+
+
+
+
+}  // namespace gst_pipes
+
+#include <pluginlib/class_list_macros.hpp>
+PLUGINLIB_EXPORT_CLASS(gst_pipes::gst_pipes_snapshooter, gst_pipes::gst_pipes_plugin)
