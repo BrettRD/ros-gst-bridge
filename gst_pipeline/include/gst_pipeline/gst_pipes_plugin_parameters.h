2d5f118285bbbb5cdbdd55c625591a61ee5736d6
BrettRD <brettrd@brettrd.com>
2023-05-25 23:39:45 +1000

    adds notes and boilerplate for parameter mappings

---
+++ gst_pipeline/include/gst_pipeline/gst_pipes_plugin_parameters.h
@@ -0,0 +1,40 @@
+#ifndef GST_PIPELINE__GST_PIPES_PLUGIN_PARAMETERS_H_
+#define GST_PIPELINE__GST_PIPES_PLUGIN_PARAMETERS_H_
+
+#include <gst_bridge/gst_bridge.h>
+
+#include "rclcpp/rclcpp.hpp"
+
+namespace gst_pipes
+{
+/*
+  This plugin declares ros parameters for props on pipeline elements
+*/
+class gst_pipes_parameters : public gst_pipes_plugin
+{
+public:
+  // during init, we need to
+  //  fetch a param from the node for the list of elements to track
+  //  find the elements in the pipeline
+  //  declare parameters for each prop on the element
+  void initialise(
+    std::string name,  // the config name of the plugin
+    std::shared_ptr<gst_bridge::node_interface_collection> node_if, GstElement * pipeline);
+
+  // callback when the pipeline adds an element
+  //  conditionally declare parameters
+  static void deep_element_added_cb(
+    GstBin * self, GstBin * sub_bin, GstElement * element, gpointer user_data);
+
+  // callback when the pipeline removes an element
+  //  undeclare any previously declared parameters
+  static void deep_element_removed_cb(
+    GstBin * self, GstBin * sub_bin, GstElement * element, gpointer user_data);
+
+private:
+  std::vector<std::string> element_names;
+};
+
+}  // namespace gst_pipes
+
+#endif  //GST_PIPELINE__GST_PIPES_PLUGIN_PARAMETERS_H_
