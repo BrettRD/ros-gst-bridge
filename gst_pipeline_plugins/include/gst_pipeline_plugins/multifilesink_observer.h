f9aa691780705b326cdf1eaddc0013f942e8ecb9
Aqeel Ahmad Akber <aqeel@thaum.io>
2023-06-29 19:13:06 +1000

    wip: first pass at a GstBus message observer

---
+++ gst_pipeline_plugins/include/gst_pipeline_plugins/multifilesink_observer.h
@@ -0,0 +1,47 @@
+#ifndef GST_PIPELINE_PLUGINS__MULTIFILESINK_OBSERVER_H_
+#define GST_PIPELINE_PLUGINS__MULTIFILESINK_OBSERVER_H_
+
+#include <gst_bridge/gst_bridge.h>
+#include <gst_pipeline/plugin_base.h>
+
+#include <gst_msgs/msg/multifilesink_event.hpp>
+
+#include "rclcpp/rclcpp.hpp"
+
+namespace gst_pipeline_plugins
+{
+/*
+  A class publishes the gstreamer multifilesink messages if post-message=true is passed as a to a topic `/{mysink}/gst_multifilesink`.
+  This can be used with `ros2 topic hz` to measure the serialisation rate of the stream
+
+    TODO build-in the functionality of ros2 topic hz to reduce chatter on the DDS
+*/
+class multifilesink_observer : public gst_pipeline::plugin_base
+{
+public:
+  // during init, we need to
+  //  fetch a param from the node for the element name of the target element
+  //  search the pipeline for the target element
+  //  insert a probe into the pipeline on the sink pad of the target element
+  void initialise(
+    std::string name,  // the config name of the plugin
+    std::shared_ptr<gst_bridge::node_interface_collection> node_if, GstElement * pipeline);
+
+  // This callback is attached to the GstBus via:
+  // https://gstreamer.freedesktop.org/documentation/gstreamer/gstbus.html?gi-language=c#gst_bus_add_watch
+  static gboolean gst_bus_cb(GstBus* bus, GstMessage* message, gpointer user_data);
+
+private:
+  // the name of the target element in the pipeline
+  std::string elem_name_;
+  std::string frame_id_;
+
+  // the publisher for the statistics messages
+  rclcpp::Publisher<gst_msgs::msg::MultifilesinkEvent>::SharedPtr event_pub_;
+
+
+};
+
+}  // namespace gst_pipeline_plugins
+
+#endif  //GST_PIPELINE_PLUGINS__MULTIFILESINK_OBSERVER_H_
---
+++ gst_pipeline_plugins/src/multifilesink_observer.cpp
@@ -0,0 +1,88 @@
+#include <type_traits>
+#include <multifilesink_observer.h>
+
+
+namespace gst_pipeline_plugins
+{
+  void multifilesink_observer::initialise(std::string name, std::shared_ptr<gst_bridge::node_interface_collection> node_if, GstElement * pipeline) {
+  name_ = name;
+  node_if_ = node_if;
+  pipeline_ = pipeline;
+
+  frame_id_ = node_if_->parameters
+  ->declare_parameter(
+                      name_ + ".frame_id", rclcpp::ParameterValue(node_if_->base->get_name()),
+                      descr("the frame_id denoting the observation", true))
+  .get<std::string>();
+
+  elem_name_ = node_if_->parameters
+                 ->declare_parameter(
+                   name_ + ".element_name", rclcpp::ParameterValue("mysink"),
+                   descr("the name of the sink element inside the pipeline", true))
+                 .get<std::string>();
+
+  auto topic_name_ = node_if_->parameters
+                ->declare_parameter(
+                  name_ + ".event_topic", rclcpp::ParameterValue("/" + elem_name_ + "/gst_multifilesink"),
+                  descr("the topic name to post events from the sink", true))
+                .get<std::string>();
+
+  rclcpp::QoS qos = rclcpp::SensorDataQoS();
+
+  event_pub_ = rclcpp::create_publisher<gst_msgs::msg::MultifilesinkEvent>(
+              node_if_->parameters, node_if_->topics, topic_name_, qos);
+
+  // Attach callback to our bus
+  // https://gstreamer.freedesktop.org/documentation/gstreamer/gstpipeline.html?gi-language=c#gst_pipeline_get_bus
+  GstBus* bus = gst_pipeline_get_bus(GST_PIPELINE(pipeline_));
+  gst_bus_add_watch(bus, &multifilesink_observer::gst_bus_cb, static_cast<gpointer>(this));
+  gst_object_unref(bus);
+
+  // bus watch messages only occur in a main loop
+
+
+}
+  gboolean multifilesink_observer::gst_bus_cb(GstBus* bus, GstMessage* message, gpointer user_data) {
+    (void)bus;
+    auto* this_ptr = static_cast<multifilesink_observer*>(user_data);
+    const GstStructure* s;
+
+    RCLCPP_INFO(this_ptr -> node_if_ -> logging -> get_logger(), "got bus callback");
+
+    switch (GST_MESSAGE_TYPE(message)) {
+    case GST_MESSAGE_ELEMENT:
+      s = gst_message_get_structure(message);
+      RCLCPP_INFO(this_ptr -> node_if_ -> logging -> get_logger(), "got bus msg");
+      if (strcmp(gst_structure_get_name(s), "GstMultiFileSink")) {
+        // Create the ROS Message
+        auto msg = gst_msgs::msg::MultifilesinkEvent();
+        // Fill the standard header
+        rclcpp::Clock::SharedPtr ros_clock = this_ptr->node_if_->clock->get_clock();
+        msg.header.stamp = ros_clock->now();
+        msg.header.frame_id = this_ptr->frame_id_;
+        // Now populate it with the GST Message
+        msg.filename = std::string(gst_structure_get_string(s, "filename"));
+        gst_structure_get_int(s, "index", &msg.index);
+        gst_structure_get_clock_time(s, "timestamp", static_cast<GstClockTime*>(&msg.timestamp));
+        gst_structure_get_clock_time(s, "stream-time", static_cast<GstClockTime*>(&msg.stream_time));
+        gst_structure_get_clock_time(s, "running-time", static_cast<GstClockTime*>(&msg.running_time));
+        gst_structure_get_clock_time(s, "duration", static_cast<GstClockTime*>(&msg.duration));
+        gst_structure_get_uint64(s, "offset", &msg.offset);
+        gst_structure_get_uint64(s, "offset-end", &msg.offset_end);
+        // Publish
+        this_ptr -> event_pub_ -> publish(msg);
+      }
+      break;
+    default:
+      RCLCPP_INFO(this_ptr -> node_if_ -> logging -> get_logger(), "message unknown");
+      /* adjust code here if segfaults */
+      break;
+    }
+
+    return true;
+  }
+
+}  // namespace gst_pipeline_plugins
+
+#include <pluginlib/class_list_macros.hpp>
+PLUGINLIB_EXPORT_CLASS(gst_pipeline_plugins::multifilesink_observer, gst_pipeline::plugin_base)
