9e6f395b2a48b6e677405df67756291f498c1f89
BrettRD <brettrd@brettrd.com>
2023-07-01 15:48:12 +1000

    adds plugin that adds data to a rtp header extension, missing linked symbols

---
+++ gst_pipeline_plugins/include/gst_pipeline_plugins/rtp_header_hook.h
@@ -0,0 +1,57 @@
+#ifndef GST_PIPELINE_PLUGINS__RTP_HEADER_HOOK_H_
+#define GST_PIPELINE_PLUGINS__RTP_HEADER_HOOK_H_
+
+#include <gst_bridge/gst_bridge.h>
+#include <gst_pipeline/plugin_base.h>
+
+#include <gst_msgs/msg/meta_mark.hpp>
+
+#include "rclcpp/rclcpp.hpp"
+
+namespace gst_pipeline_plugins
+{
+/*
+    Inject or retrieve metadata into a buffer at a certain pad
+
+*/
+class rtp_header_hook : public gst_pipeline::plugin_base
+{
+public:
+  // during init, we need to
+  //  fetch a param from the node for the element name of the target element
+  //  search the pipeline for the target element
+  //  insert a probe into the pipeline on the sink pad of the target element
+  void initialise(
+    std::string name,  // the config name of the plugin
+    std::shared_ptr<gst_bridge::node_interface_collection> node_if, GstElement * pipeline);
+
+  // This callback is run inside the pad of the sink element, the return will be ok or drop.
+  //  This function needs to be static, we can pass a pointer to our logic in user-data
+  static GstPadProbeReturn mark_cb(
+    GstPad * pad, GstPadProbeInfo * info, gpointer user_data);
+
+  static GstPadProbeReturn report_cb(
+    GstPad * pad, GstPadProbeInfo * info, gpointer user_data);
+
+private:
+  // the name of the target element in the pipeline
+  std::string elem_name_;
+
+  // a pointer to the target element in the pipeline
+  GstElement * bin_;
+
+  // the topic to publish on
+  std::string topic_name_;
+  std::string frame_id_;
+
+  // whether we mark the buffer or report on its marking
+  bool mark_;
+
+  // the publisher for the statistics messages
+  rclcpp::Publisher<gst_msgs::msg::MetaMark>::SharedPtr mark_pub_;
+
+};
+
+}  // namespace gst_pipeline_plugins
+
+#endif  //GST_PIPELINE_PLUGINS__RTP_HEADER_HOOK_H_
---
+++ gst_pipeline_plugins/src/rtp_header_hook.cpp
@@ -0,0 +1,215 @@
+#include <rtp_header_hook.h>
+
+#include <gst/rtp/gstrtpbuffer.h>
+
+
+#include "meta/gstmark.h"
+
+namespace gst_pipeline_plugins
+{
+void rtp_header_hook::initialise(
+  std::string name,  // the config name of the plugin
+  std::shared_ptr<gst_bridge::node_interface_collection> node_if,
+  GstElement * pipeline)
+{
+  name_ = name;
+  node_if_ = node_if;
+  pipeline_ = pipeline;
+
+  elem_name_ = node_if->parameters
+                 ->declare_parameter(
+                   name_ + ".element_name", rclcpp::ParameterValue("mysrc"),
+                   descr("the name of the source element inside the pipeline", true))
+                 .get<std::string>();
+
+  mark_ = node_if->parameters
+                 ->declare_parameter(
+                   name_ + ".mark", rclcpp::ParameterValue(true),
+                   descr("mark the buffer (true) or report (false)", true))
+                 .get<bool>();
+
+  topic_name_ = node_if->parameters
+                ->declare_parameter(
+                  name_ + ".report_topic", rclcpp::ParameterValue("~/" + name_ + "/metamark"),
+                  descr("the topic name to post events from the source", true))
+                .get<std::string>();
+
+  frame_id_ = node_if_->parameters
+    ->declare_parameter(
+                        name_ + ".frame_id", rclcpp::ParameterValue(node_if_->base->get_name()),
+                        descr("the frame_id denoting the reporting frame", true))
+    .get<std::string>();
+
+  rclcpp::QoS qos = rclcpp::SensorDataQoS();
+
+  mark_pub_ = rclcpp::create_publisher<gst_msgs::msg::MetaMark>(
+              node_if->parameters, node_if->topics, topic_name_, qos);
+
+
+  if (GST_IS_BIN(pipeline_)) {
+    bin_ = gst_bin_get_by_name(GST_BIN_CAST(pipeline_), elem_name_.c_str());
+    if (bin_) {
+      RCLCPP_INFO(
+        node_if->logging->get_logger(), "plugin rtp_header_hook '%s' found '%s'",
+        name_.c_str(), elem_name_.c_str());
+
+      // find the src pad of the element
+      GstPad * pad;
+
+      if(mark_){
+        // bind to the source of the rtp payloader to edit the newly generated header
+        pad = gst_element_get_static_pad(bin_, "src");
+        gst_pad_add_probe(
+          pad, GST_PAD_PROBE_TYPE_BUFFER_LIST,
+          (GstPadProbeCallback)rtp_header_hook::mark_cb, static_cast<gpointer>(this), NULL);
+      }
+      else
+      {
+        // bind to the sink of the rtp de-payloader to retrieve the header
+        pad = gst_element_get_static_pad(bin_, "sink");
+        gst_pad_add_probe(
+          pad, GST_PAD_PROBE_TYPE_BUFFER,
+          (GstPadProbeCallback)rtp_header_hook::report_cb, static_cast<gpointer>(this), NULL);
+      }
+
+    }
+
+    else {
+      RCLCPP_ERROR(
+        node_if->logging->get_logger(),
+        "plugin rtp_header_hook '%s' failed to locate a gstreamer element called '%s'",
+        name_.c_str(), elem_name_.c_str());
+    }
+  } else {
+    RCLCPP_ERROR(
+      node_if->logging->get_logger(),
+      "plugin rtp_header_hook '%s' received invalid pipeline in initialisation",
+      name_.c_str());
+  }
+}
+
+GstPadProbeReturn rtp_header_hook::mark_cb(
+  GstPad * pad, GstPadProbeInfo * info, gpointer user_data)
+{
+  GstPadProbeReturn ret;
+  (void)pad;
+  auto this_ptr = static_cast<rtp_header_hook*>(user_data);
+
+  GstBuffer *buf = GST_PAD_PROBE_INFO_BUFFER(info);
+  gboolean ext_added = false;
+
+  GstClockTime timestamp = GST_BUFFER_PTS(buf);
+  gpointer payload = &timestamp;
+  size_t payload_size = sizeof(timestamp);
+
+  guint8 appbits = 1; // Application specific bits
+  guint8 header_ext_id = 1;  //The ID of the header extension
+
+  // https://stackoverflow.com/questions/67603364/rtp-extension-headers-over-udp
+  GstRTPBuffer rtp_buf = GST_RTP_BUFFER_INIT;
+  gst_rtp_buffer_map(buf, GST_MAP_READWRITE, &rtp_buf);
+  ext_added = gst_rtp_buffer_add_extension_twobytes_header(
+    &rtp_buf,
+    appbits,
+    header_ext_id,
+    payload,
+    payload_size);
+  gst_rtp_buffer_unmap(&rtp_buf);
+
+
+  if(!ext_added){
+    RCLCPP_ERROR(
+      this_ptr->node_if_->logging->get_logger(),
+      "plugin rtp_header_hook '%s' failed to add a header extension",
+      this_ptr->name_.c_str());
+  }
+
+  // life is peachy
+  ret = GST_PAD_PROBE_OK;
+  return ret;
+}
+
+
+GstPadProbeReturn rtp_header_hook::report_cb(
+  GstPad * pad, GstPadProbeInfo * info, gpointer user_data)
+{
+  GstPadProbeReturn ret;
+  (void)pad;
+  auto this_ptr = static_cast<rtp_header_hook*>(user_data);
+
+  GstBuffer *buf = GST_PAD_PROBE_INFO_BUFFER(info);
+
+  GstClockTime timestamp;
+
+  gboolean ext_found = false;
+  guint8 appbits; // Application specific bits
+  guint8 header_ext_id = 1;  //filter for header extensions matching this id.
+  guint nth = 0; //fetch the first matching payload
+  gpointer payload;
+  guint payload_size = sizeof(timestamp);
+
+  // https://stackoverflow.com/questions/67603364/rtp-extension-headers-over-udp
+  GstRTPBuffer rtp_buf = GST_RTP_BUFFER_INIT;
+  gst_rtp_buffer_map(buf, GST_MAP_READ, &rtp_buf);
+  ext_found = gst_rtp_buffer_get_extension_twobytes_header(
+    &rtp_buf,
+    &appbits,
+    header_ext_id,
+    nth,
+    &payload,
+    &payload_size
+  );
+  gst_rtp_buffer_unmap(&rtp_buf);
+
+
+  // unpack the payload according to our ext_id
+  memcpy(&timestamp, payload, sizeof(timestamp));
+
+
+
+
+  if(ext_found){
+    GstClockTime tx_pts = timestamp;
+    GstClockTime rx_pts = GST_BUFFER_PTS(buf);
+
+    // XXX the rtp depayloader doesn't set the timestamp,
+    //     stick on any old thing in this case, we only need it to be unique
+    //     Improve this by interrogating the latency of the pipeline
+    if(rx_pts == GST_CLOCK_TIME_NONE){
+      GstClock* pipe_clock = gst_pipeline_get_pipeline_clock (GST_PIPELINE_CAST(this_ptr->pipeline_));
+      GstClockTime gst_clock_time = gst_clock_get_time(pipe_clock);
+      GstClockTime base_time = gst_element_get_base_time(GST_ELEMENT(this_ptr->pipeline_));
+      rx_pts = gst_clock_time - base_time;
+      GST_BUFFER_PTS(buf) = rx_pts;
+    }
+
+
+    // pack a ros message
+    auto msg = gst_msgs::msg::MetaMark();
+
+    msg.header.stamp = this_ptr->node_if_->clock->get_clock()->now();
+    msg.header.frame_id = this_ptr->frame_id_;
+
+    msg.buffer_pts = rx_pts;
+    msg.mark_timestamp = tx_pts;
+
+    this_ptr -> mark_pub_->publish(msg);
+  }
+  else
+  {
+    RCLCPP_ERROR(
+      this_ptr->node_if_->logging->get_logger(),
+      "plugin rtp_header_hook '%s' could not find a matching header extension",
+      this_ptr->name_.c_str());
+  }
+
+  // life is peachy
+  ret = GST_PAD_PROBE_OK;
+  return ret;
+}
+
+
+}  // namespace gst_pipeline_plugins
+
+#include <pluginlib/class_list_macros.hpp>
+PLUGINLIB_EXPORT_CLASS(gst_pipeline_plugins::rtp_header_hook, gst_pipeline::plugin_base)
